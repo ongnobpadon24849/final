9f6ea70ca704213c0106957d9f8c2cc3d939fabc
Noppadon Panboonyeun <ongnobpadon24849@gmail.com>
2024-09-30 18:54:17 +0700

    Edit files

--- functiontest.py
+++ functiontest.py
@@ -9,30 +9,29 @@ class FlaskTestCase(unittest.TestCase):
     def setUp(self):
         self.tester = api_app.test_client()
 
-    def test_true_when_x_is_17(self):
-        response = self.tester.get('/is_prime/17')
+    def test_x_is_3dot6(self):
+        response = self.tester.get('/is1honor/3.6')
         data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Is_prime', data)
-        self.assertEqual(data.get('Is_prime'), True)
-        print(f"Test(/is_prime/17) ==> Response: {data}")
+        self.assertIn('is1honor', data)
+        self.assertEqual(data.get('is1honor'), True)
+        print(f"Test(/is1honor/3.6) ==> Response: {data}")
 
-    def test_true_when_x_is_13219(self):
-        response = self.tester.get('/is_prime/13219')
+    def test_x_is_2dot0(self):
+        response = self.tester.get('/is1honor/2.0')
         data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Is_prime', data)
-        self.assertEqual(data.get('Is_prime'), True)
-        print(f"Test(/is_prime/13219) ==> Response: {data}")
+        self.assertIn('is1honor', data)
+        self.assertEqual(data.get('is1honor'), False)
+        print(f"Test(/is1honor/3.5) ==> Response: {data}")
 
-    def test_false_when_x_is_36(self):
-        response = self.tester.get('/is_prime/36')
+    def test_x_is_5dot1(self):
+        response = self.tester.get('/is1honor/5.1')
         data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Is_prime', data)
-        self.assertEqual(data.get('Is_prime'), False)
-        print(f"Test(/is_prime/36) ==> Response: {data}")
-        
+        self.assertIn('ERROR', data)
+        self.assertEqual(data.get('ERROR'), "Invalid input")
+        print(f"Test(/is1honor/5.1) ==> Response: {data}")
 
 if __name__ == '__main__':
     unittest.main()
